101221b556c306fd7431d6ee7e292236310922ff
Yuvan Michael Vivenzi <yuvanmichael@outlook.com>
2025-08-06 13:08:16 +0530

    Initial commit

---
+++ C/main.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+    int a; // Variable declaration
+    a = 5; // Variable initialization
+    printf("Hello, World!");
+    printf("%d", a);
+    printf("The output of this program is %d", a);
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Dev
+All my dev work
