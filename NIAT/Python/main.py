ee70ea3163fd3ef854a155c7305bb787eb02388a
Yuvan Michael Vivenzi <yuvanmichael@outlook.com>
2025-08-28 14:02:40 +0530

    Update main.py

--- NIAT/Python/main.py
+++ NIAT/Python/main.py
@@ -1,3 +1,10 @@
-a = input()
-b = len(a) * "*"
-print(b)
+a = int(input())
+b = int(input())
+c = a ** b
+d = b ** a
+if c > d:
+    print("a to the power of b is greater")
+    print(c)
+else:
+    print("b to the power of a is greater")
+    print(d)
